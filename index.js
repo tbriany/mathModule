787c4ff7448f1abcfc7b42978914b3202127a996
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-11-05 20:53:12 -0500

    added mul to index

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
+const mul = require('./multiply.js');
+
 const Math = {
     PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
   }
