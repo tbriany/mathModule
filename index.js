c806d029934a79a5d066118d8444ea315c6fa26a
Michael D Amparo <michaelamparo@pursuit.org>
2019-11-05 21:05:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
   This is a collaboratively made Math module with support for basic math operations.
   Feel free to use it for you basic math needs.
 */
-const divide = require("./divide.js")
+const div = require("./divide.js")
 const mul = require('./multiply.js');
-const subtract = require('./subtraction.js')
+const sub = require('./subtraction.js')
 const sum = require('./sum.js')
 
 const Math = {
     PI: 3.141592653589793, // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
-    divide,
+    div,
     sum, 
-    subtract,
+    sub,
     mul
   }
 
