50a83e7ee74e0b9b25eecbc98537976a68aade28
tbriany <brianytaveras@pursuit.org>
2019-11-05 19:25:43 -0500

    mathModule start file

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+  myMathModule
+
+  This is a collaboratively made Math module with support for basic math operations.
+  Feel free to use it for you basic math needs.
+*/
+const Math = {
+    PI: 3.141592653589793 // Mathematical constant defined as the ratio of a circle's circumference to its diameter.
+  }
+  
+  module.exports = Math
